5492dcf0179ed95bf0a22bdc563642569de3416d
Jan Wolski <jan@wolski.fi>
2016-03-06 19:23:34 +0200

    AST parser & AJAX get

---
+++ src/code/astParser.js
@@ -0,0 +1,56 @@
+import AstNode from './AstNode'
+
+var tokenizer = /(;;[^\n]*)|("[^"]*")|(\()|(\))|([^\s\(\)]+)/g;
+var commentRex = /^;;/g
+
+var tokens
+
+function astParser(str){
+    tokens = new TokenList()
+    var incoming
+    
+    while(incoming = tokenizer.exec(str)){
+        if(commentRex.test(incoming[0])) continue // filter out comments
+        tokens.addToken(incoming[0])
+    }
+    var first = tokens.next()
+    
+    if(first !== '(' || first === null) throw new SyntaxError('AST parse error: string must start with "("')
+    return parseNode()
+}
+
+function parseNode(parent){
+    var type = tokens.next() // TODO: check that not paren!
+    
+    var current = new AstNode(type, parent)
+    var incoming
+    while(( incoming = tokens.next() ) !== ')'){
+        if(incoming === null) throw new SyntaxError('AST parse error: unexpected end of input')
+        if(incoming === '('){
+            current.addChild( parseNode(current) )
+        }else{
+            current.addChild(new AstNode(incoming, current))
+        }
+    }
+    return current
+}
+
+class TokenList{
+    constructor(){
+        this.tokens = []
+        this.index = 0
+    }
+    addToken(token){
+        this.tokens.push(token)
+    }
+    next(){
+        if(this.index < this.tokens.length){
+            return this.tokens[this.index++]
+        }else{
+            this.index = 0
+            return null
+        }
+    }
+}
+
+export default astParser
--- src/code/index.jsx
+++ src/code/index.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AstNodeComponent from './AstNodeComponent'
 import AstNode from './AstNode'
+import astParser from './astParser'
 
-var stored = window.localStorage.getItem('ast')
+var stored //= window.localStorage.getItem('ast')
 var rootNode
 
 if(stored){
@@ -15,24 +16,38 @@ if(stored){
 rootNode.setFrozen(true)
 
 var RootComponent = React.createClass({
+    getInitialState(){
+        return {root: this.props.root}
+    },
     handleNotify(){
         this.forceUpdate()
     },
     componentDidMount(){
-        this.timer = setInterval( ()=>{
+       /* this.timer = setInterval( ()=>{
             var json = AstNode.stringify(this.props.root)
             window.localStorage.setItem('ast', json)
             console.log ('Saving to localStorage: ', json )
-        } , 5000)
+        } , 5000)*/
+        AJAX('https://raw.githubusercontent.com/WebAssembly/spec/master/ml-proto/test/switch.wast', (response) => {
+            var newRoot = astParser(response)
+            newRoot.setFrozen(true)
+            this.setState({root: newRoot})
+        })
     },
     componentWillUnmount(){
         clearInterval(this.timer)
     },
     render(){
-        return <AstNodeComponent node={this.props.root} notifyUp={this.handleNotify}/>
+        return <AstNodeComponent node={this.state.root} notifyUp={this.handleNotify}/>
     }
 })
 
 
 ReactDOM.render(<RootComponent root={rootNode} />, document.getElementById('wastedit-main'))
 
+function AJAX(url, success) {
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", function(){success(this.responseText)});
+    oReq.open("GET", url);
+    oReq.send();
+}
