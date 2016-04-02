3f4fb6424682d51463b2bda605a9d29bb1bcab1f
Jan Wolski <jan@wolski.fi>
2016-04-02 14:57:17 +0300

    Mobile styles

--- src/code/HeaderBar.jsx
+++ src/code/HeaderBar.jsx
@@ -7,12 +7,12 @@ var HeaderBar = React.createClass({
     render(){
         return (
         <div className="header-bar">
-            <h1>ast.run</h1><h2>WebAssembly playground</h2>
             <div className="header-ui">
                 <DropdownMenu label="Example:" options={this.props.examples} onSelect={this.props.onExampleChange}/>
                 <DropdownMenu label="Text format:" options={formats} onSelect={this.props.onFormatChange}/>
                 <InfoPopup />
             </div>
+            <h1>ast.run</h1><h2>WebAssembly playground</h2>
         </div>
         )
     }
