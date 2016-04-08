a77999f0238bdd27807b9a7953eb6e0db5dae1e0
Jan Wolski <jan@wolski.fi>
2016-04-08 21:40:56 +0300

    Creator link

--- src/code/InfoPopup.jsx
+++ src/code/InfoPopup.jsx
@@ -30,7 +30,7 @@ var InfoPopup = React.createClass({
             <tr><td><strong>Tab:</strong></td><td> Add new node after current node</td></tr>
             </tbody>
             </table>
-            <div className='button-row'><p className='credits'>Created by Jan Wolski. Powered by <a href="https://github.com/WebAssembly/binaryen" target="_blank">binaryen</a>.</p><button onClick={this.handleClick} onMouseDown={prevent}>Got it!</button></div>
+            <div className='button-row'><p className='credits'>Created by <a href="https://github.com/data-ux/" target="_blank">Jan Wolski</a>. Powered by <a href="https://github.com/WebAssembly/binaryen" target="_blank">binaryen</a>.</p><button onClick={this.handleClick} onMouseDown={prevent}>Got it!</button></div>
             </div>
             )
         return <div className="info-holder">{this.state.open ? popup : null}<button onClick={this.handleClick} onMouseDown={prevent} className={this.state.open?'active':''}>info</button></div>
